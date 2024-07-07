f103f0e09dcb7ea3b44ebaad469ce2cb386db09b
Zhuofan <654905617@qq.com>
2024-07-07 23:23:46 +0800

    lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,12 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesHard(){
+        IntList lst = IntList.of(13, 14, 17);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("169 -> 14 -> 289", lst.toString());
+        assertTrue(changed);
+    }
 }
